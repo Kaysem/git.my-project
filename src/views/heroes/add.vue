837f4e72dde28d69394749f653409ab819e0a39a
Kaysem <929200707@qq.com>
2018-06-03 22:06:59 +0800

    完成添加英雄

--- src/views/heroes/add.vue
+++ src/views/heroes/add.vue
@@ -4,23 +4,44 @@
       <form>
           <div class="form-group">
             <label for="heroesname">英雄名称</label>
-            <input type="text" class="form-control" id="heroesname" placeholder="英雄名称">
+            <input type="text" v-model="formData.name" class="form-control" id="heroesname" placeholder="英雄名称">
           </div>
           <div class="form-group">
             <label for="heroesgender">英雄性别</label>
-            <input type="text" class="form-control" id="heroesgender" placeholder="英雄性别">
+            <input type="text" v-model="formData.gender" class="form-control" id="heroesgender" placeholder="英雄性别">
           </div>
-          <button type="submit" class="btn btn-success">提交</button>
+          <button @click.prevent="handleAdd()" type="submit" class="btn btn-success">提交</button>
       </form>
     </div>
 </template>
 
 <script>
+// 导入axios
+import axios from 'axios';
 export default {
   data() {
     return {
-      add: []
+      formData: {
+        name: '',
+        gender: ''
+      }
     };
+  },
+  methods: {
+    handleAdd() {
+      axios.post('http://localhost:3000/heroes', this.formData)
+        .then((res) => {
+          if (res.status === 201) {
+            // 添加成功
+            this.$router.push({
+              name: 'heroes'
+            });
+          };
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }
 };
 </script>
