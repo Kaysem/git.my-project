b7155c7188c5071ec673180668b833387f74e451
Kaysem <929200707@qq.com>
2018-06-03 18:03:52 +0800

    全局组件(nav+侧边栏)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+// 程序的入口
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+
+// 导入bootstrap样式
+import 'bootstrap/dist/css/bootstrap.css';
+// 导入自定义样式
+import '@/assets/css/index.css';
 
 Vue.config.productionTip = false;
 
